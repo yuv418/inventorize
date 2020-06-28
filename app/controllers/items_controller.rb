8e2217fd1182b3c7669e5b53eed04664abec9d26
Ramesh Balaji <post2rb@hotmail.com>
2020-06-28 18:38:46 -0400

    Update to add easier (and just access) to item actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,45 @@ class ItemsController < ApplicationController
     @item = Item.find params[:id]
   end
 
+  def index
+    if query_params[:category_id]
+      @items = Category.find(query_params[:category_id]).items.where(query_params.except(:category_id))
+    else
+      @items = Item.where(query_params)# TODO actually query stuff
+    end
+  end
+
+  def edit
+    @item = Item.find params[:id]
+    @schema = @item.schema
+  end
+
+  def update
+
+    @item = Item.find params[:id]
+    @item.categories = permit_params[:categories].map { |id_s| Category.find(id_s) }
+
+    if @item.update(permit_params.except :categories)
+      redirect_to @item
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @item = Item.find params[:id]
+    @item.destroy
+
+    redirect_to items_path
+  end
+
   private
   def permit_params
     params.require(:item).permit(:schema_id, data: [], categories: [])
+  end
+
+  def query_params
+    params.permit(:schema_id, :category_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :schemas, controller: :schema
   resources :categories
-  resources :items, except: :index
+  resources :items
 
   root to: "categories#index"
 end
